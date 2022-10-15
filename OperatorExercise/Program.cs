9f248ce074026408118d0de0eca94d4902cd88f1
Susan Winters <susanwinters214@gmail.com>
2022-10-15 14:23:55 -0500

    added some code

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,32 @@
 ﻿namespace OperatorExercise
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("EXERCISE 1:");
+          
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if(a == 17 && b==4)
+            {
+                 Console.WriteLine($"17/4 is {quotient} + {remainder}");              
+            }
+
+            Console.WriteLine("EXERCISE 2:");
+            Console.WriteLine("What is the radius of your circle?");
+           var radius = double.Parse(Console.ReadLine());
+            Console.WriteLine(AreaOfCircle(radius));
+        }
+
+        
+        public static double AreaOfCircle(double radius)
+        {
+            return (Math.PI * radius * radius); 
         }
     }
 }
